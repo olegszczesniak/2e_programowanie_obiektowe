034f39fe65b9f6ffd11c6e0b00b59e82dce38f9c
olegszczesniak <olegszczesniak0@gmail.com>
2023-10-03 15:14:27 +0200

    zadanie2

---
+++ 03_10_2023/zadanie2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b , h , x;
+
+	cout << "Menu: \n 1- kwadrat \n 2- trojkat \n 3- romb \n 4-trapez " << endl << endl;
+
+	cout << "Podaj figure:";
+	cin >> x;
+
+	switch (x)
+	{
+	case 1:
+		cout << "Podaj bok: ";
+		cin >> a;
+		cout << "Pole kwadratu:" << a * a;
+
+	}
+}
