af8cf61ebdeb27c99dc7e33c951f6097a805a26e
Oleg Szcxesniak <109421550+olegszczesniak@users.noreply.github.com>
2023-10-17 15:12:06 +0200

    Add files via upload

---
+++ 17_10_2023/zadanie1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	string login = "login1";
+	string haslo = "haslo1";
+	int bledy = 3;
+
+	do {
+		cout << "Wprowadz login: ";
+		cin >> login;
+
+		if (login != "login1")
+		{
+			cout << "Bledny login!\n";
+			bledy--;
+		}
+		else
+		{
+			do {
+				cout << "Wprowadz haslo: ";
+				cin >> haslo;
+
+				if (haslo != "haslo1")
+				{
+					cout << "Bledne haslo!\n";
+					bledy--;
+				}
+				else
+				{
+					cout << "Zalogowano";
+					goto exit;
+				}
+			} while (bledy > 0);
+		}
+		if (bledy == 0)
+		{
+			cout << "Zablokowano mozliwosc logowania";
+		}
+	} while (bledy > 0);
+
+	return 0;
+	
+}
---
+++ 17_10_2023/zadanie2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	int y = 1;
+	cout << "Wprowadz liczbe parzysta: (0 konczy dzialanie) ";
+	cin >> x;
+	if (x == 0)
+	{
+		cout << "Nie wprowadzono zadnej liczby";
+		goto exit;
+	}
+	do {
+		if (x % 2 != 0)
+		{
+			do {
+				cout << "Wprowadzono liczbe nieparzysta, wprowadz parzysta: ";
+				cin >> x;
+			}while (x % 2 != 0);
+		}
+		if (x == 0)
+		{
+			cout << "Iloczyn wprowadzonych liczb jest rowny: " << y;
+			goto exit;
+		}
+		y = y * x;
+		cout << "Wprowadz liczbe parzysta: (0 konczy dzialanie) ";
+		cin >> x;
+	} while (x % 2 == 0);
+
+	return 0;
+}
