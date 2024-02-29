9164bc71b98b4b9f8b0fd90edacf3d0879c152eb
Oleg Szcxesniak <109421550+olegszczesniak@users.noreply.github.com>
2024-02-29 13:16:33 +0100

    Add files via upload

---
+++ 29.02.2024/zad.2.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+using namespace std;
+
+void pobierzLancuch(string& lancuch) {
+	cin >> lancuch;
+}
+
+void sumaLancuchow(string lancuch1, string lancuch2, string& lancuch3)
+{
+	lancuch3 = lancuch 1 + " " + lancuch2;
+}
+
+void wyswietlLancuch(string lancuch)
+{
+	cout << lancuch << endl;
+}
+
+int main() {
+	string nazwaJezyka = "", wersjaJezyka = "";
+	string jezyk = "";
+
+	cout << "podaj nazwe jezyka programowania: ";
+	pobierzLancuch(nazwaJezyka);
+
+
+	cout << "podaj wersje";
+	pobierzLancuch(wersjaJezyka);
+
+	sumaLancuchow(nazwaJezyka, wersjaJezyka, jezyk);
+
+	cout << "Jezyk Programowania: " ;
+	wyswietlLancuch(jezyk);
+
+	reurn 1;
+}
+
+
+
+
+
+
+
+
---
+++ 29.02.2024/zad1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void pobierzLancuch(string& lancuch) {
+	cin >> lancuch;
+}
+
+void sumaLancuchow(string lancuch1, string lancuch2, string& lancuch3)
+{
+	lancuch3 = lancuch 1 + " " + lancuch2;
+}
+
+void wyswietlLancuch (string lancuch)
+
+
+
+
+
+
+
+
