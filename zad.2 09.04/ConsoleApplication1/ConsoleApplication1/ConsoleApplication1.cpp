5fc6776e44f413c7e454b4eec24b44f6d0669892
Oleg Szcxesniak <109421550+olegszczesniak@users.noreply.github.com>
2024-04-09 15:14:20 +0200

    Add files via upload

---
+++ zad.2 09.04/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+
+#include <fstream> 
+
+
+using namespace std;
+
+
+
+int main(void) {
+
+	ifstream plik("plik.txt");
+
+	if (plik.is_open()) {
+
+		string linia, zawartosc;
+		char znak;
+
+		while (!plik.eof()) {
+
+			znak = plik.get();
+
+
+			if (plik.good())
+
+				cout << znak << endl;
+		}
+	}
+
+	else {
+
+		cout << "Nie mozna otworzyc pliku." << endl;
+
+	}
+	system("PAUSE");
+	return EXIT_SUCCESS;
+
+}
