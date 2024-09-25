8a4eebb1dc91b9d2b2659a1fe2140fe31d480080
Oleg Szcxesniak <109421550+olegszczesniak@users.noreply.github.com>
2024-09-25 13:19:34 +0200

    Add files via upload

---
+++ 25.09.24/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+/*
+ // Zadanie 1
+Console.WriteLine("Witaj świecie!");
+
+// Zadanie 2
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj swoje imię:  ");
+        string imie = Console.ReadLine();
+        Console.WriteLine("Cześć " + imie + "!");
+    }
+}
+
+// Zadanie 3
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        int c = Convert.ToInt32(a);
+        int d = Convert.ToInt32(b);
+        Console.WriteLine(c + d);
+
+    }
+}
+
+//Zadanie 4
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        double r = double.Parse(a);
+        Console.WriteLine(double.Pi * (r * r));
+    }
+}
+
+
+//Zadanie 5
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine((c * 9/5) + 32);
+    }
+}
+
+
+//Zadanie 6 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+        Console.WriteLine(b);
+    }
+}
+
+
+
+//Zadanie 7
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine( (a+b) / 2);
+    }
+}
+
+//Zadanie 8 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());  
+        Console.WriteLine(a * a);
+    }
+}
+*/
+//Zadanie 9 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());  
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(b);
+    }
+}
+
+//KONIEC
+
+
+
